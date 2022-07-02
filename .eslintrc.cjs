b4616a94b7f8077288c4f26e691441a3f3dc71c5
hjun-park <phj0860@gmail.com>
2022-07-02 22:34:28 +0900

    ESLint, Prettier 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,33 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ]
-}
+	root: true,
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-prettier',
+	],
+	env: {
+		'vue/setup-compiler-macros': true,
+	},
+	// 001. eslint 설정
+	// 개발환경에만 허용하고 실제론 eslint 콘솔 금지 룰 추가
+	rules: {
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+			},
+		],
+	},
+};
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { fileURLToPath, URL } from 'url'
+import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      // 001. alias 를 지정하는 방법 '@/components/TheWelcome.vue' 이런 식으로 alias 지정 가능
-      '@': fileURLToPath(new URL('./src', import.meta.url))   
-    }
-  }
-})
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			// 001. alias 를 지정하는 방법 '@/components/TheWelcome.vue' 이런 식으로 alias 지정 가능
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+});

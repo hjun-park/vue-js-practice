635ef8e4b13364e35ebdee61063f8822bb447934
220597 <hyunjunpark@hansol.com>
2022-08-10 16:49:47 +0900

    [Props] 부모에서 자식으로 데이터를 전달하는 Props

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -3,12 +3,15 @@
 		<!-- 3개씩 배치-->
 		<div class="card">
 			<div class="card-body">
-				<h5 class="card-title red">Card title</h5>
-				<p class="card-text">
-					Some quick example text to build on the card title and make up the
-					bulk of the card's content.
-				</p>
-				<a href="#" class="btn btn-primary">Go somewhere</a>
+				<!-- type : news, notice -->
+				<span class="badge bg-secondary">{{
+					type === 'news' ? '뉴스' : '공지사항'
+				}}</span>
+				<!-- script에서 정의한 props는 여기서 사용, 머스태치로 감싼다.-->
+				<h5 class="card-title red mt-2">{{ title }}</h5>
+				<p class="card-text">{{ contents }}</p>
+				<a v-if="isLike" href="#" class="btn btn-danger">좋아요</a>
+				<a v-else href="#" class="btn btn-outline-danger">좋아요</a>
 			</div>
 		</div>
 	</div>
@@ -16,14 +19,48 @@
 
 <script>
 export default {
+	// props: ['title', 'contents'],
+	/*
+    props: 컴포넌트에 등록할 수 있는 사용자 정의 속성
+      => 부모 컴포넌트에서 자식 컴포넌트로 데이터 전달 가능
+  */
+	// 반응형 prop 데이터 생성
+	/*
+    type: 자료형
+    default: 기본값
+    required: 속성값 필수여부
+    validator: 속성값의 유효성 검사
+  */
+	props: {
+		type: {
+			type: String,
+			default: 'news',
+			validator: value => {
+				return ['news', 'notice'].includes(value);
+			},
+		},
+		title: {
+			type: String,
+			required: true,
+		},
+		contents: {
+			type: String,
+			required: true,
+		},
+		isLike: {
+			type: Boolean,
+			default: false,
+		},
+		obj: {
+			type: Object,
+			// 기본값을 함수 형태로도 반환할 수 있다.
+			default: () => {},
+		},
+	},
 	setup() {
 		return {};
 	},
 };
 </script>
 
-<style lang="scss" scoped>
-.red {
-	color: v-bind(color) !important;
-}
-</style>
+<style lang="scss" scoped></style>
--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -6,12 +6,19 @@
 		<div class="container text-center py-4">
 			<div class="row g-3">
 				<!-- card 사이 간격 부여 (gx, gy) -->
-				<div class="col col-4"><AppCard></AppCard></div>
-				<div class="col col-4"><AppCard></AppCard></div>
-				<div class="col col-4"><AppCard></AppCard></div>
-				<div class="col col-4"><AppCard></AppCard></div>
-				<div class="col col-4"><AppCard></AppCard></div>
-				<div class="col col-4"><AppCard></AppCard></div>
+				<!-- v-bind로 props 동적인 데이터 전달 방법 -->
+				<!-- 부모에서 전달 시에는 kebab-case를 사용할 것-->
+				<div v-for="post in posts" :key="post.id" class="col col-4">
+					<AppCard
+						:title="post.title"
+						:contents="post.contents"
+						:type="post.type"
+						:is-like="post.isLike"
+					></AppCard>
+				</div>
+
+				<!-- 정의한 props에 정적인 데이터 전달 방법 (부모 -> 자식)-->
+				<!-- <AppCard title="제목 1" contents="내용 1"></AppCard></div> -->
 			</div>
 		</div>
 	</main>
@@ -19,10 +26,38 @@
 
 <script>
 import AppCard from '@/components/AppCard.vue';
+import { reactive } from 'vue';
 export default {
 	components: { AppCard },
 	setup() {
-		return {};
+		// 정의한 props에 동적인 데이터 전달 방법 (객체 정의 및 return)
+		const post = reactive({
+			title: '제목 2',
+			contents: '내용 2',
+		});
+
+		// loop를 돌면서 반응형 객체 순회
+		const posts = reactive([
+			{ id: 1, title: '제목1', contents: '내용1', isLike: true, type: 'news' },
+			{ id: 2, title: '제목2', contents: '내용2', isLike: true, type: 'news' },
+			{ id: 3, title: '제목3', contents: '내용3', isLike: true, type: 'news' },
+			{
+				id: 4,
+				title: '제목4',
+				contents: '내용4',
+				isLike: false,
+				type: 'notice',
+			},
+			{
+				id: 5,
+				title: '제목5',
+				contents: '내용5',
+				isLike: false,
+				type: 'notice',
+			},
+		]);
+
+		return { post, posts };
 	},
 };
 </script>

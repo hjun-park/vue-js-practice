66a52c55191b7e25ada1eb0bdd2a7c02bfd6eda3
220597 <hyunjunpark@hansol.com>
2022-08-10 15:21:17 +0900

    [컴포넌트 기초 + 심화] Vue3 컴포넌트 기초

---
+++ src/components/TheView.vue
@@ -0,0 +1,30 @@
+<template>
+	<!-- 02. main 코드 작성 -->
+	<!-- File name rule : 파스칼 형식을 따르며 
+  하나의 컴포넌트만 가진 경우라면 The로 시작한다.-->
+	<main>
+		<div class="container text-center py-4">
+			<div class="row g-3">
+				<!-- card 사이 간격 부여 (gx, gy) -->
+				<div class="col col-4"><AppCard></AppCard></div>
+				<div class="col col-4"><AppCard></AppCard></div>
+				<div class="col col-4"><AppCard></AppCard></div>
+				<div class="col col-4"><AppCard></AppCard></div>
+				<div class="col col-4"><AppCard></AppCard></div>
+				<div class="col col-4"><AppCard></AppCard></div>
+			</div>
+		</div>
+	</main>
+</template>
+
+<script>
+import AppCard from '@/components/AppCard.vue';
+export default {
+	components: { AppCard },
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
+import AppCard from '@/components/AppCard.vue';
 
-createApp(App).mount('#app');
+// 컴포넌트를 전역적으로 등록하는 방법
+// 전역으로 등록하면 어디서든 컴포넌트 사용 가능
+// 전역등록은 사용 비추
+const app = createApp(App).mount('#app');
+// app.component('AppCard', AppCard);
+
+app.mount('#app');
+
 import 'bootstrap/dist/js/bootstrap.js';
-
-console.log('Hello');

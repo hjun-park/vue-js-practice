32b3542f8846aa0d5ff422f67b2def205554f391
220597 <hyunjunpark@hansol.com>
2022-08-11 18:06:06 +0900

    프로젝트 초기화

---
+++ vue-advance/vue3-posts/src/layouts/TheView.vue
@@ -0,0 +1,18 @@
+<template>
+	<!-- 01-02. App.vue에 있는 Main을 컴포넌트로 분리 -->
+	<main>
+		<div class="container py-4">
+			<h2>Hello Vue3</h2>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
